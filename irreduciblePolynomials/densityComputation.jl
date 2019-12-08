6d94067fe8c3c71644d691a7cefac8f3bdc6f3b4
ercprk <kyoungdukp@gmail.com>
2019-12-08 00:36:57 -0500

    Implemented approximating density of irreducible integer polynomials

--- irreduciblePolynomials/densityComputation.jl
+++ irreduciblePolynomials/densityComputation.jl
@@ -2,18 +2,49 @@
 # Asymptotic Density Project
 # Nov 22nd, 2019
 
-# RANDOM_SAMPLE_SIZE::Int64 = 500
-
 module DensityComputation
 
     using Nemo
     using DataFrames
     using Printf
-    using Distributed
+    using Random
 
-
+    # Function  : irrIntPolyBrute
+    # Arguments : desired maxDegree and maxConstant of polynomials over integer
+    # Returns   : a Dataframe of maxDegree, maxConstant, the total number of
+    #             polynomials, the total number of irreducible polynomials,
+    #             and the density of irreducible polynomials in all polynomials
+    # Does      : Brute forces to compute all polynomials over Z of a set
+    #             maximum degree and maximum constant. Computes the
+    #             irreducibility of all polynomials and density of irreducible
+    #             polynomials in all polynomials.
     function
-    calculateDensity(maxDegree, maxConstant, df)::DataFrame
+    irrIntPolyBrute(maxDegree::Int64, maxConstant::Int64)::DataFrame
+
+        # Set x as the univariable of the polynomial ring over Z
+        S, x = PolynomialRing(ZZ, 'x')
+
+        # Initialize the dataframe with a zero polynomial
+        df = DataFrame(polynomial = [0*x^0], irreducibility = [true])
+
+        # Populate the dataframe with the rest of the zero-degree polynomials
+        for constant in range(1, step = 1, stop = maxConstant)
+            push!(df::DataFrame, [constant * x ^ 0, true])
+        end
+
+        # Populate the dataframe with the rest of the polynomials
+        for degree in range(1, step = 1, stop = maxDegree)
+            for constant in range(1, step = 1, stop = maxConstant)
+                polynomial = constant * x ^ degree
+                for i in range(1, step = 1, stop = (maxConstant + 1) ^ degree)
+                    push!(df, [polynomial + df[i, 1],
+                          isirreducible(polynomial + df[i, 1])])
+                end
+            end
+        end
+
+        # Compute the total number of all polynomials, irreducible polynomials,
+        # and density
         numPoly = DataFrames.nrow(df)
         numIrrd = sum(df[!, 2])
         density = numIrrd / numPoly
@@ -25,36 +56,63 @@ module DensityComputation
         return densityDf
     end
 
-    # Function  :
-    # Arguments :
-    # Returns   :
-    # Does      :
+    # Function  : irrIntPolyApprox
+    # Arguments : number of steps
+    # Returns   : a Dataframe with each row:
+    #             1) step number
+    #             2) cumulative number of polynomials
+    #             3) cumulative number of irreducible polynomials
+    #             4) cumulative density
+    # Does      : Let the term PolyZ_m be the set of all polynomials over Z
+    #             with maxDegree <= m and maxConstant <= m. At each step number
+    #             m, n polynomials from PolyZ_m are picked at random and
+    #             computed their irreducibility, where n = max{|PolyZ_m|, 500}.
+    #             Thus, asymptotic density of irreducible polynomials in all
+    #             polynomials over Z is cumulatively approximated as m
+    #             approaches infinity.
     function
-    irrIntPolyBrute(maxDegree::Int64, maxConstant::Int64)::DataFrame
+    irrIntPolyApprox(numSteps::Int64)::DataFrame
 
-
-        # Quotient Ring Z_{maxConstant + 1} / <x^{maxDegree}>
-        #R = ResidueRing(ZZ, maxConstant + 1)
+        # Set x as the univariable of the polynomial ring over Z
         S, x = PolynomialRing(ZZ, 'x')
 
-        # Initialize the dataframe with a zero polynomial
-        df = DataFrame(polynomial = [0*x^0], irreducibility = [true])
+        # Initialize the dataframe with step 0
+        numPoly = 1
+        numIrrPoly = 1
+        df = DataFrame(step = [0], numPoly = [numPoly],
+                       numIrrPoly = [numIrrPoly], density = [1.000])
 
-        #Populate the dataframe with the rest of the zero-degree polynomials within
-        #the ring Z_{maxConstant}/<x^{maxDegree}>
-        for constant in range(1, step = 1, stop = maxConstant)
-            push!(df::DataFrame, [constant * x ^ 0, true])
-        end
+        # Seed random number generate
+        Random.seed!(0)
 
-        for degree in range(1, step = 1, stop = maxDegree)
-            for constant in range(1, step = 1, stop = maxConstant)
-                polynomial = constant * x ^ degree
-                for i in range(1, step = 1, stop = (maxConstant + 1)^degree)
-                    push!(df, [polynomial + df[i, 1], isirreducible(polynomial + df[i, 1])])
+        # Approximates the density of irreducible polynomials in all
+        # polynomials over Z
+        for m in range(1, step = 1, stop = numSteps)
+            @printf("Step: %0.0f\n", m)
+
+            # Size of PolyZ_m
+            sizePolyZ_m = (m + 1) ^ (m + 1)
+
+            # Number of polynomials to be computed
+            n = m > 4 ? 500 : sizePolyZ_m
+            @printf("n: %0.0f\n", n)
+
+            for _  in range(1, step = 1, stop = n)
+                polynomial = 0*x^0
+                for degree in range(m, step = -1, stop = 0)
+                    polynomial += rand(0 : m) * x ^ degree
                 end
+                if isirreducible(polynomial)
+                    numIrrPoly += 1
+                end
+                numPoly +=1
             end
+
+            density = Float64(numIrrPoly) / Float64(numPoly)
+            push!(df, [m, numPoly, numIrrPoly, density])
         end
 
-        return calculateDensity(maxDegree, maxConstant, df)
+        return df
     end
+
 end
--- irreduciblePolynomials/main.jl
+++ irreduciblePolynomials/main.jl
@@ -1,40 +1,15 @@
 include("densityComputation.jl")
 import .DensityComputation
-using DataFrames, Printf, CSV
+using DataFrames, Printf, CSV, Plots
 
 function main()
-    # Approximates the Asymptotic density of irreducible polynomials in all
-    # integer polynomials. This is done by setting a number representing both
-    # maxDegree and maxConstant to infinity, but
-    # practically to 20.
-    irrIntPolyBruteDf = DataFrames.DataFrame(maxDegree = [0],
-                                             maxConstant = [0],
-                                             numPolynomials = [1],
-                                             numIrreducibles = [1],
-                                             density = [1.0])
+    # Approximates the asymptotic density of irreducible polynomials in all
+    # polynomials over integer.
+    irrIntPolyDf = DensityComputation.irrIntPolyApprox(20)
+    CSV.write("irrIntPoly.csv", irrIntPolyDf, writeheader = true)
 
-    for i in range(1, step = 1, stop = 15)
-        @printf("maxDegree, maxConstant = %0.0f\n", i)
-        thisDf = DensityComputation.irrIntPolyBrute(i, i)
-        irrIntPolyBruteDf = join(irrIntPolyBruteDf, thisDf, kind = :outer,
-                            on = intersect(names(irrIntPolyBruteDf), names(thisDf)))
-    end
-    print(irrIntPolyBruteDf)
-    CSV.write("irrIntPolyBrute.csv", irrIntPolyBruteDf, writeheader = true)
-    
-
-
-    # for maxDegree in range(1, step = 1, stop = 5)
-    #     @printf("maxDegree: %.0f\n", maxDegree)
-    #     for i in range(1, step = 1, stop = 10)
-    #         # Set maximum constant of polynomials as i-th prime from 0,
-    #         # subtracted by 1 (technical reason for Nemo)
-    #         maxConstant = Primes.nextprime(0, i) - 1
-    #         @printf("maxConstant: %.0f\n", maxConstant)
-    #         df = irreduciblePolynomials(maxDegree, maxConstant)
-    #         calculateDensity(df)
-    #     end
-    # end
+    plot(x = irrIntPolyDf[!, 4], y = irrIntPolyDf[!, 1])
+    savefig("output.png")
 end
 
 main()
